349b858e70ffbfd1eda6e4ab16bbfd9f4119f88d
dollboy <hanww1215@hanmail.net>
2016-07-06 16:29:51 +0900

    revise

--- fork_full_request.c
+++ fork_full_request.c
@@ -2,12 +2,13 @@
 
 int main()
 {
-  int i;
-  for (i = 0; i < 5; i++) {
-    for (j = 0; j < 9; j++) {
-      printf("%dx%d=%d ", i, j, i * j);
+  int i, j;
+  for (i = 2; i <= 9; i++)
+ {
+    for (j = 1; j <= 9; j++)
+    {
+      printf("%d x %d = %d\n ", i, j, i * j);
     }
   }
-  
   return 0;
 }
